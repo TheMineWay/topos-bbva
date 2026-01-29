1d737b5598910cce04d2d3c878cd514b83214d84
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-29 17:01:27 +0100

    feat: start building registration form

---
+++ src/features/register/components/registration-manager.tsx
@@ -0,0 +1,10 @@
+import { Paper, Title } from "@mantine/core";
+import styles from "./registration-manager.module.css";
+
+export const RegistrationManager: FC = () => {
+    return <Paper shadow="md" p="lg" className={styles.root}>
+        <Title className="center">Register</Title>
+        
+        {/* Form */}
+    </Paper>;
+}
