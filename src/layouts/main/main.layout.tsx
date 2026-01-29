b7b45baeea5a95139dbb5a5183d6a5a92840ec30
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-29 16:38:20 +0100

    feat: main layout

---
+++ src/layouts/main/main.layout.tsx
@@ -0,0 +1,9 @@
+import styles from './main.layout.module.css';
+
+type Props = {
+    children?: React.ReactNode;
+}
+
+export const MainLayout: FC<Props> = ({ children }) => {
+    return <main className={styles.container}>{children}</main>;
+};
