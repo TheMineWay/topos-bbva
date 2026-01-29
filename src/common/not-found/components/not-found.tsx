0b97153c3ee250d7b9b89c7a7a7e8ef0bcd1af5f
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-29 16:43:43 +0100

    feat: redirect to / if not found route

---
+++ src/common/not-found/components/not-found.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
+
+export const NotFound: FC = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate({ to: "/" });
+    }, [navigate]);
+
+    return null;
+};
