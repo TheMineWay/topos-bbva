b69ce31c4da73c4512ab557471e9db3e969f08b9
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-30 12:54:52 +0100

    fix: missing title translation

--- src/features/register/components/registration-manager.tsx
+++ src/features/register/components/registration-manager.tsx
@@ -7,8 +7,12 @@ import {
 } from "./form/registration.form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useTranslation } from "react-i18next";
+import { TRANSLATIONS } from "@i18n/translation.enum";
 
 export const RegistrationManager: FC = () => {
+	const { t } = useTranslation(TRANSLATIONS.REGISTER);
+
 	const form = useForm<RegistrationFormValues>({
 		resolver: zodResolver(REGISTRATION_FORM_SCHEMA),
 		reValidateMode: "onChange",
@@ -16,7 +20,7 @@ export const RegistrationManager: FC = () => {
 
 	return (
 		<Paper shadow="md" p="lg" className={styles.root}>
-			<Title className="center">Register player</Title>
+			<Title className="center">{t("Title")}</Title>
 			<RegistrationForm form={form} onSuccess={(data) => console.log(data)} />
 		</Paper>
 	);
