56288bf41f2cdb0f292a52746ff3a9263ebda3fa
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-30 09:15:18 +0100

    feat: PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { resolve } from "path";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
@@ -30,6 +31,47 @@ export default defineConfig(({mode}) => {
 				babel: {
 					plugins: [["babel-plugin-react-compiler"]],
 				},
+			}),
+			VitePWA({
+				registerType: 'autoUpdate',
+				devOptions: {
+					enabled: true
+				},
+
+				// Custom manifest data
+				includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+				manifest: {
+					name: 'Topos BBVA',
+					short_name: 'Topos',
+					description: 'Juego Topos BBVA',
+					theme_color: '#1212BA',
+					icons: [
+						{
+							src: 'icons/pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png'
+						},
+						{
+							src: 'icons/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png'
+						},
+
+						// Maskable icons
+						{
+							src: 'icons/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any'
+						},
+						{
+							src: 'icons/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'maskable'
+						}
+					],
+				}
 			}),
 		],
 		server: {
