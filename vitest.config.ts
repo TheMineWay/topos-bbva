3be1dd30ce5ca7ae8af8d373c67a9e072b90b812
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-30 14:40:27 +0100

    feat: add GH test pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({});
+export default defineConfig({
+	test: {
+		reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
+	},
+});
