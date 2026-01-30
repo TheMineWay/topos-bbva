322dd43bc3c7a03e2701f2e133b8ae76eacd3da1
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-30 12:35:22 +0100

    fix: missing node prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { resolve } from "path";
+import { resolve } from "node:path";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
