cd1db2afe9e05980da2f90455e0dd39c4e4bd026
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-30 19:07:10 +0100

    feat: test register flow

--- e2e/metadata/title.spec.ts
+++ e2e/metadata/title.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
+import { E2E_CONSTANTS } from "../e2e.constants";
 
 test("Has correct title", async ({ page }) => {
-	await page.goto("http://localhost:3000");
+	await page.goto(E2E_CONSTANTS.BASE_URL);
 
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle("Topos BBVA");
