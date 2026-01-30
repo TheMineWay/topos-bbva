8dd5441efe584af72a775b48a7d5bff5282a98f8
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-30 08:14:43 +0100

    feat: allow hosts via .env so I can connect using ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { resolve } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-	resolve: {
-		alias: {
-			"@assets": resolve(__dirname, "src/assets"),
-			"@common": resolve(__dirname, "src/common"),
-			"@features": resolve(__dirname, "src/features"),
-			"@layouts": resolve(__dirname, "src/layouts"),
-		},
-	},
-	plugins: [
-		tanstackRouter({
-			target: "react",
-			autoCodeSplitting: true,
-		}),
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]],
+export default defineConfig(({mode}) => {
+	// Load env
+	const ENV = loadEnv(mode, process.cwd(), "");
+
+	// Allowed hosts (for dev server)
+	const allowedHosts = ENV.VITE_ALLOWED_HOSTS?.split(",");
+
+	return {
+		resolve: {
+			alias: {
+				"@assets": resolve(__dirname, "src/assets"),
+				"@common": resolve(__dirname, "src/common"),
+				"@features": resolve(__dirname, "src/features"),
+				"@layouts": resolve(__dirname, "src/layouts"),
 			},
-		}),
-	],
+		},
+		plugins: [
+			tanstackRouter({
+				target: "react",
+				autoCodeSplitting: true,
+			}),
+			react({
+				babel: {
+					plugins: [["babel-plugin-react-compiler"]],
+				},
+			}),
+		],
+		server: {
+			allowedHosts,
+		}
+	};
 });
