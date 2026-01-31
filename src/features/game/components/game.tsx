c094bfd4f3cbcf9f06180ade656a738443482b86
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-31 09:36:40 +0100

    feat: game actions

--- src/features/game/components/game.tsx
+++ src/features/game/components/game.tsx
@@ -1,8 +1,32 @@
+import { Button } from "@mantine/core";
 import { useBoard } from "../hooks/use-board";
 import { Board } from "./board";
+import { useTranslation } from "react-i18next";
+import { TRANSLATIONS } from "../../../i18n/translation.enum";
+import { useCallback } from "react";
+import styles from "./game.module.css";
 
 export const Game: FC = () => {
+	const { t } = useTranslation(TRANSLATIONS.GAME);
 	const manager = useBoard();
 
-	return <Board manager={manager} />;
+	const onActionClick = useCallback(() => {
+		if (manager.isPlaying) manager.stop();
+		else manager.play();
+	}, [manager.isPlaying, manager.play, manager.stop]);
+
+	return (
+		<div className={styles.root}>
+			<Board manager={manager} />
+
+			<Button
+				size="xl"
+				onClick={onActionClick}
+				className={styles["action-button"]}
+				data-testid={manager.isPlaying ? "stop-button" : "play-button"}
+			>
+				{t(`actions.${manager.isPlaying ? "Stop" : "Play"}`)}
+			</Button>
+		</div>
+	);
 };
