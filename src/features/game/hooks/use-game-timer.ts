44df2b276cacf55571de882952aeafd9cbb229c8
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-31 12:21:08 +0100

    fix: timer reset bug

--- src/features/game/hooks/use-game-timer.ts
+++ src/features/game/hooks/use-game-timer.ts
@@ -1,4 +1,7 @@
-import { useEffect } from "react";
+import { addMilliseconds, isAfter } from "date-fns";
+import { useEffect, useState } from "react";
+
+const INTERVAL = 50;
 
 type Options = {
 	tickDelay?: number; // In milliseconds
@@ -6,8 +9,26 @@ type Options = {
 };
 
 export const useGameTimer = ({ tickDelay = 1000, onTick }: Options = {}) => {
+	const [targetTime, setTargetTime] = useState<Date>(
+		addMilliseconds(new Date(), tickDelay),
+	);
+
 	useEffect(() => {
-		const unsubscribe = setInterval(() => onTick?.(), tickDelay);
+		const unsubscribe = setInterval(() => {
+			const now = new Date();
+			if (isAfter(now, targetTime)) {
+				onTick?.();
+				setTargetTime(addMilliseconds(now, tickDelay));
+			}
+		}, INTERVAL);
 		return () => clearInterval(unsubscribe);
-	}, [onTick, tickDelay]);
+	}, [onTick, tickDelay, targetTime]);
 };
+
+/**
+ * DEV NOTE
+ *
+ * Could not create an interval of "tickDelay". That created a bug. As the useEffect was recalculated, it was like the
+ * timer was reseted.
+ * I decided to store the target time and check every X time (INTERVAL).
+ */
