533ed47490352c1f78c9c007ea6eaa9935476481
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-31 15:08:28 +0100

    feat: greet user

--- src/layouts/game/game.layout.tsx
+++ src/layouts/game/game.layout.tsx
@@ -1,15 +1,25 @@
-import { Paper } from "@mantine/core";
+import { AppShell, Text } from "@mantine/core";
 import styles from "./game.layout.module.css";
+import { useUser } from "../../providers/user/use-user";
+import { useTranslation } from "react-i18next";
+import { TRANSLATIONS } from "../../i18n/translation.enum";
 
 type Props = {
 	children: React.ReactNode;
 };
 
 export const GameLayout: FC<Props> = ({ children }) => {
+	const { t } = useTranslation(TRANSLATIONS.GAME);
+	const { user } = useUser();
+
 	return (
-		<div className={styles.layout}>
-			<Paper className={styles.header}></Paper>
+		<AppShell className={styles.layout}>
+			<AppShell.Header className={styles.header}>
+				<Text>{t("layout.Greeting", { name: user?.username })}</Text>
+			</AppShell.Header>
+
+			{/* Content */}
 			<div className={styles.content}>{children}</div>
-		</div>
+		</AppShell>
 	);
 };
