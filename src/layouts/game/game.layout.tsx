58b6da890b665d9bbda9a639d36f708f62f83a62
Joel Campos <joelcamposoliva9000@gmail.com>
2026-01-31 15:11:18 +0100

    feat: user can now logout

--- src/layouts/game/game.layout.tsx
+++ src/layouts/game/game.layout.tsx
@@ -1,8 +1,10 @@
-import { AppShell, Text } from "@mantine/core";
+import { ActionIcon, AppShell, Group, Text } from "@mantine/core";
 import styles from "./game.layout.module.css";
 import { useUser } from "../../providers/user/use-user";
 import { useTranslation } from "react-i18next";
 import { TRANSLATIONS } from "../../i18n/translation.enum";
+import { IconLogout2 } from "@tabler/icons-react";
+import { useCallback } from "react";
 
 type Props = {
 	children: React.ReactNode;
@@ -10,12 +12,23 @@ type Props = {
 
 export const GameLayout: FC<Props> = ({ children }) => {
 	const { t } = useTranslation(TRANSLATIONS.GAME);
-	const { user } = useUser();
+	const { user, setUser } = useUser();
+
+	const onLogout = useCallback(() => setUser(null), [setUser]);
 
 	return (
 		<AppShell className={styles.layout}>
 			<AppShell.Header className={styles.header}>
-				<Text>{t("layout.Greeting", { name: user?.username })}</Text>
+				<Group gap="xs">
+					<ActionIcon
+						variant="transparent"
+						onClick={onLogout}
+						data-testid="logout-button"
+					>
+						<IconLogout2 />
+					</ActionIcon>
+					<Text>{t("layout.Greeting", { name: user?.username })}</Text>
+				</Group>
 			</AppShell.Header>
 
 			{/* Content */}
